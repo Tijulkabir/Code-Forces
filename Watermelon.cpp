191c5e0f2cf588bba44e11a022c4eabed51c8b7a
Tijulkabir <tijulkabir4@gmail.com>
2024-12-14 21:02:25 +0600

    CP

---
+++ Watermelon.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+
+    cin >> w;
+    if (1<=w && w<=100)
+    {
+        
+    if ( w>2 && w%2==0)
+    {
+        cout << "YES";
+    }
+    
+    else
+    { 
+    cout << "NO";
+    }
+
+    }
+
+ 
+    
+  return 0;
+}
